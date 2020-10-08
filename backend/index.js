00be4ffe45b40ec8cac7c44ec875a35854969b89
clust3rr <thg.santos@outlook.com>
2020-10-08 18:12:13 -0300

    created tables on database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
-const app = express()
 const consign = require('consign')
+const db = require('./config/db')
+const app = express()
+
+app.db = db
 
 consign()
     .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
     .into(app)
 
 app.listen(3000, () => {
