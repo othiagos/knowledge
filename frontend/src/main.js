2a75baee536adde57890d873ad2bbf3d6e8d5d9d
clust3rr <thg.santos@outlook.com>
2020-10-24 19:40:50 -0300

    added responsiveness to the menu

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,12 @@ import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 
 import App from './App'
+
 import './config/bootstrap'
 import './config/msgs'
 import './config/axios'
+import './config/mq'
+
 import store from './config/store'
 import router from './config/router'
 
