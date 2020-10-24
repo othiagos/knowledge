37dc32fc6497537f23b23ecbfb14bc63c69020c0
clust3rr <thg.santos@outlook.com>
2020-10-24 18:41:09 -0300

    validating token

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,64 @@
 <template>
 	<div id="app" :class="{'hide-menu': !isMenuVisible}">
 		<Header 
-			title="Base de Conhecimento" 
+			title="Knowledge" 
 			:hideToggle="!user"
 			:hideUserDropdown="!user"/>
 		<Menu v-if="user" />
-		<Content />
+		<Loading v-if="validatingToken"/>
+		<Content v-else />
 		<Footer />
 	</div>
 </template>
 
 <script>
+import axios from 'axios'
+import { baseApiUrl, userKey } from '@/global'
 import { mapState } from 'vuex' 
 import Header from '@/components/templates/Header'
 import Menu from '@/components/templates/Menu'
 import Content from '@/components/templates/Content'
 import Footer from '@/components/templates/Footer'
+import Loading from '@/components/templates/Loading'
 
 export default {
 	name: "App",
-	components: { Header, Menu, Content, Footer },
-	computed: mapState(['isMenuVisible', 'user'])
+	components: { Header, Menu, Content, Footer, Loading },
+	computed: mapState(['isMenuVisible', 'user']),
+	data: function() {
+		return {
+			validatingToken: true
+		}
+	},
+	methods: {
+		async validateToken() {
+			this.validatingToken = true
+
+			const json = localStorage.getItem(userKey)
+			const userData = JSON.parse(json)
+			this.$store.commit('setUser', null)
+
+			if(!userData) {
+				this.validatingToken = false
+				this.$router.push({ name: 'auth' })
+				return
+			}
+
+			const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+
+			if (res.data) {
+				this.$store.commit('setUser', userData)
+			} else {
+				localStorage.removeItem(userKey)
+				this.$router.push({ name: 'auth' })
+			}
+
+			this.validatingToken = false
+		}
+	},
+	created() {
+		this.validateToken()
+	}
 }
 </script>
 
